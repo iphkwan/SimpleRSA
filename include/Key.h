a6faea6698d706eda92b30a762978ccfc978e300
Chung Yu-tang <zonyitoo@gmail.com>
2013-01-01 13:54:25 +0800

    forget to add headfiles

---
+++ include/Key.h
@@ -0,0 +1,13 @@
+#ifndef KEY_H_
+#define KEY_H_
+
+#include "BigInt.h"
+class Key
+{
+    private:
+        // pass
+    public:
+        // pass
+};
+
+#endif
---
+++ include/RSA.h
@@ -0,0 +1,18 @@
+#ifndef RSA_H_
+#define RSA_H_
+
+#include <string>
+#include <iostream>
+#include "BigInt.h"
+#include "Key.h"
+using namespace std;
+
+class RSA
+{
+    private: 
+        // pass
+    public:
+        // pass
+};
+
+#endif
