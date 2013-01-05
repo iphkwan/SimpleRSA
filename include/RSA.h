9134ee87c17b099da440783a1939d110bbb01241
iphkwan <yexjun@gmail.com>
2013-01-05 15:52:44 +0800

    Little fix.

--- include/RSA.h
+++ include/RSA.h
@@ -10,40 +10,40 @@ using namespace std;
 //通过getPublicKey把公钥取走，然后调用encrypt加密信息吧。
 class RSA
 {
-private: 
-    BigInt n,d;
-	BigInt e;
-public:
-	RSA(int digNum = 512)
-	{
-		Key key(digNum / 2);
-		key.getPrivateKey(n, d);
-		key.getPublicKey(n, e);
-	}
-    RSA(BigInt n, BigInt e, BigInt d) 
-    {
-        this->n = n;
-        this->e = e;
-        this->d = d;
-    }
-	BigInt encrypt(BigInt src)
-	{
-		return PowerMode(src, e, n);
-	}
-	BigInt decrypt(BigInt src)
-	{
-		return PowerMode(src, d, n);
-	}
-	void getPublicKey(BigInt& N, BigInt& E)
-	{
-		N = this->n;
-		E = this->e;
-	}
-	void getPrivateKey(BigInt& N,BigInt& D)
-	{
-		N = this->n;
-		D = this->d;
-	}
+    private: 
+        BigInt n,d;
+	    BigInt e;
+    public:
+	    RSA(int digNum = 512)
+	    {
+		    Key key(digNum / 2);
+		    key.getPrivateKey(n, d);
+		    key.getPublicKey(n, e);
+	    }
+        RSA(BigInt n, BigInt e, BigInt d) 
+        {
+            this->n = n;
+            this->e = e;
+            this->d = d;
+        }
+	    BigInt encrypt(BigInt src)
+	    {
+		    return PowerMode(src, e, n);
+	    }
+	    BigInt decrypt(BigInt src)
+	    {
+		    return PowerMode(src, d, n);
+	    }
+	    void getPublicKey(BigInt& N, BigInt& E)
+	    {
+		    N = this->n;
+		    E = this->e;
+	    }
+	    void getPrivateKey(BigInt& N,BigInt& D)
+	    {
+		    N = this->n;
+		    D = this->d;
+	    }
 };
 
 #endif
