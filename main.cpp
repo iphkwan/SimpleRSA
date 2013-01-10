bfc2a4be7fa3790702fa47d7d1c313c1aece12b3
iphkwan <yexjun@gmail.com>
2013-01-10 14:24:32 +0800

    For merge.

--- main.cpp
+++ main.cpp
@@ -31,8 +31,8 @@ int main()
     cout << "Hello RSA project!\n";
     //test_BigInt();
     //test_RSA();
-    //test_OAEP();
-    test_stringTrans();
+    test_OAEP();
+    //test_stringTrans();
     //test_example();
     //test_primeGen();
     //test_fastKeyGen();
