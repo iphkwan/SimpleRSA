19463368fcb16bc282923ab8b266ba8f94a96e26
19thhell <yangxh92@gmail.com>
2013-01-18 16:32:49 +0800

    StringTrans support UTF-8 char,to debug:BigInt,ToString,etc

--- src/StringTrans.cpp
+++ src/StringTrans.cpp
@@ -12,12 +12,18 @@ StringTrans::StringTrans(const string& a, int b) {
 }
 
 void StringTrans::split(const string& msg, int BitLen) {
-    int n = 0;
+    int n = 0,i = 0,j = 0;
     int StrLen = BitLen / 8;
     (*this).clear();
+    const char* msg_c = msg.c_str();
     do
     {
-        string segment = msg.substr(n * StrLen, StrLen);
+        char seg_c[255];
+        for (i = n * StrLen,j = 0;i < msg.length() && i < (n + 1) * StrLen;i++,j++)
+            seg_c[j] = msg_c[i];
+        seg_c[j] = '\0';
+        string segment(seg_c);
+        cout << "Segment: " << segment << endl;
         push_back(BigInt(segment, 10));
         n++;
     }
